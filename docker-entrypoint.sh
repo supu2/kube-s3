e8715dcd51bc9396e72c5c8ca1684aa0b3b22aa1
supu2 <supu2@protonmail.com>
2024-10-10 14:52:24 +0300

    fix mnt opt

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,4 +8,4 @@ export S3_ACL=${S3_ACL:-private}
 
 mkdir -p ${MNT_POINT}
 
-s3fs  ${S3_BUCKET} ${MNT_POINT} -d -f -o ${MNT_OPTS}
+s3fs  ${S3_BUCKET} ${MNT_POINT} -d -f -o url=${S3_URL} -o ${MNT_OPTS}
